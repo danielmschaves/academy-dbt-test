008a0980710a7ec0056c716c8624b9034169936d
danielmschaves <danielmschaves@gmail.com>
2024-11-13 21:29:34 -0300

    bridge table and tests

---
+++ models/marts/bridge_motivo_venda.sql
@@ -0,0 +1,19 @@
+-- marts/dim/bridge_motivo_venda.sql
+with stg_pedido_motivo_venda as (
+   select * from {{ ref('stg_sap__pedido_motivo_venda') }}
+),
+stg_motivo_venda as (
+   select * from {{ ref('stg_sap__motivo_venda') }}
+),
+bridge_motivo_venda as (
+   select
+       {{ dbt_utils.generate_surrogate_key(['pmv.ID_PEDIDO', 'mv.ID_MOTIVO_VENDA']) }} as SK_BRIDGE_MOTIVO
+       , pmv.ID_PEDIDO
+       , mv.ID_MOTIVO_VENDA
+       , mv.NOME_MOTIVO
+       , mv.TIPO_MOTIVO
+   from stg_pedido_motivo_venda pmv
+   left join stg_motivo_venda mv 
+       using (ID_MOTIVO_VENDA)
+)
+select * from bridge_motivo_venda
